a917ec76129d46f50923aacfc3db5a27fb4931e9
池村 和剛 <kazutakai@tt-com.net>
2016-10-23 11:15:16 +0900

    Add Logger library, and test

--- app/src/main/java/com/ikmr/banbara23/logger_sample/MainActivity.java
+++ app/src/main/java/com/ikmr/banbara23/logger_sample/MainActivity.java
@@ -1,7 +1,9 @@
 package com.ikmr.banbara23.logger_sample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.orhanobut.logger.Logger;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_scrolling);
+
+        Logger.d("Test comment");
+
+        putLog();
+    }
+
+    private void putLog() {
+        Logger.e(new Exception(),"エラー");
+        putJsonLog();
+        putTagLog();
+    }
+
+    private void putJsonLog() {
+        Logger.json("{\"date\":\"10月23日(日)\",\"temperature\":\"32℃[+1] 28℃[-1]\",\"wave\":\"2メートル後1.5メートル\",\"weather\":\"晴後雨\",\"wind\":\"南西の風後北の風\"}");
+    }
+
+    private void putTagLog() {
+        Logger.t("TAG").d(this.getClass().getCanonicalName());
     }
 }
